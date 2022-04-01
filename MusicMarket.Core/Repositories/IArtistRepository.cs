3acd9a5fefe00d36a67385e6df7ab0508301837f
Hüseyin Dönmez <deavepup@gmail.com>
2022-04-01 21:42:01 +0300

    App

---
+++ MusicMarket.Core/Repositories/IArtistRepository.cs
@@ -0,0 +1,15 @@
+﻿using MusicMarket.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Core.Repositories
+{
+    public interface IArtistRepository:IRepository<Artist>
+    {
+        Task<IEnumerable<Artist>> GetAllWithMusicsAsync();
+        Task<Artist> GetWithMusicsByIdAsync(int id);
+    }
+}
